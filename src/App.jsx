82211f22621fafdebb6930aff25bc6cfba16771c
nicerjs23 <nicerjs23@gmail.com>
2024-10-25 08:30:04 +0000

    배포

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import { ThemeProvider } from "styled-components";
+import { theme } from "@styles/theme";
+import GlobalStyle from "@styles/global";
+
+import { RouterProvider } from "react-router-dom";
+import { router } from "./router";
+
+function App() {
+  return (
+    <ThemeProvider theme={theme}>
+      <GlobalStyle />
+      <RouterProvider router={router} />
+    </ThemeProvider>
+  );
+}
+
+export default App;
