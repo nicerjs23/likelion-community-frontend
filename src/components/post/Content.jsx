1e2ad276679e636fa78cb6ad1bc16636c28e2d83
nakyeong <yunna3425@naver.com>
2024-12-01 04:59:44 +0900

    Fix: 커뮤니티에서 글 작성 직후 -1분이라고 뜨는 오류 수정

--- src/components/post/Content.jsx
+++ src/components/post/Content.jsx
@@ -17,6 +17,7 @@ export const Content = ({ id, title, body, images, likes_count, scraps_count, ti
     const now = new Date();
     const elapseTime = Math.floor((now - date) / (1000 * 60));
 
+    if (elapseTime < 0) return `0분 전`;
     if (elapseTime < 60) return `${elapseTime}분 전`;
     if (elapseTime < 1440) return `${Math.floor(elapseTime / 60)}시간 전`;
     return `${Math.floor(elapseTime / 1440)}일 전`;
